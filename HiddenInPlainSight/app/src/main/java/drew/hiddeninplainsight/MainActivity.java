2bb869d1e67cfa016089f5b54028e5ba1ced3e50
drew <mortonwd@appstate.edu>
2016-11-08 16:50:34 -0500

    Transferring to my account

---
+++ HiddenInPlainSight/app/src/main/java/drew/hiddeninplainsight/MainActivity.java
@@ -0,0 +1,7 @@
+package drew.hiddeninplainsight;
+
+/**
+ * Created by drew on 11/8/16.
+ */
+public class MainActivity {
+}
---
+++ HiddenInPlainSight/app/src/main/java/drew/hiddeninplainsight/SplashScreen.java
@@ -0,0 +1,37 @@
+package drew.hiddeninplainsight;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by drew on 11/8/16.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
